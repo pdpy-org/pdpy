169aa8437087b44ef8eca4afa2010a5dee715118
fdch <fch226@nyu.edu>
2021-10-26 18:27:38 -0300

    added vscode-pdpy stuff

---
+++ vscode-pdpy/README.md
@@ -0,0 +1,19 @@
+# pdpy Extension for VS Code
+
+vscode-pdpy is a [pdpy](https://github.com/fdch/pdpy) extension
+for [Visual Studio Code](https://code.visualstudio.com/).
+The extension currently supports text highlighting.
+
+## Features
+
+- Syntax highlighting
+
+## Future Improvements
+
+- Improved text highlighting
+- Auto-complete
+
+## License
+
+vscode-pdpy is licensed under the
+[MIT License](https://opensource.org/licenses/MIT).
---
+++ vscode-pdpy/extension.js
@@ -0,0 +1,85 @@
+const vscode = require('vscode');
+const path = require('path');
+
+
+// let _activeTerminal = null;
+// vscode.window.onDidCloseTerminal((terminal) => {
+//     if (terminal.name === 'pdpy') {
+//         if (!terminal.tckDisposed) {
+//             disposeTerminal();
+//         }
+//     }
+// });
+// function createTerminal() {
+//     _activeTerminal = vscode.window.createTerminal('pdpy');
+//     return _activeTerminal;
+// }
+// function disposeTerminal() {
+//     _activeTerminal.tckDisposed = true;
+//     _activeTerminal.dispose();
+//     _activeTerminal = null;
+// }
+// function getTerminal() {
+//     if (!_activeTerminal) {
+//         createTerminal();
+//     }
+
+//     return _activeTerminal;
+// }
+// // END TERMINAL
+
+// function resolve(editor, command) {
+//     const scPath = vscode.workspace.getConfiguration().get('pdpy.sclangCmd');
+//     return command
+//         .replace(/\${file}/g, `${editor.document.fileName}`)
+//         .replace(/\${sclangCmd}/g, scPath)
+// }
+
+// function run(command) {
+//     const terminal = getTerminal();
+
+//     terminal.show(true);
+
+//     vscode.commands.executeCommand('workbench.action.terminal.scrollToBottom');
+//     terminal.sendText(command, true);
+// }
+
+// function warn(msg) {
+//     console.log('pdpy.execInTerminal: ', msg)
+// }
+
+// function handleInput(editor) {
+//     vscode.workspace.saveAll(false);
+//     let command = "${sclangCmd} ${file}";
+//     const cmd = resolve(
+//         editor,
+//         command
+//     )
+
+//     run(cmd);
+// }
+
+// function activate(context) {
+//     let execInTerminal = vscode.commands.registerCommand('pdpy.execInTerminal', () => {
+//         const editor = vscode.window.activeTextEditor
+//         if (!editor) {
+//             warn('no active editor');
+//             return;
+//         }
+
+//         handleInput(editor)
+//     });
+//     context.subscriptions.push(execInTerminal);
+
+//     let killTerminal = vscode.commands.registerCommand('pdpy.killTerminal', () => {
+//         if(_activeTerminal)
+//             disposeTerminal();
+//     });
+//     context.subscriptions.push(killTerminal);
+// }
+// exports.activate = activate;
+
+// function deactivate() {
+//     disposeTerminal();
+// }
+// exports.deactivate = deactivate;
