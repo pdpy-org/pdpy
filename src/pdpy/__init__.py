222f220060924bf6621fe5dc6d5395435c11fc12
fdch <fch226@nyu.edu>
2021-11-05 13:13:12 -0300

    organized imports in init file

--- src/pdpy/__init__.py
+++ src/pdpy/__init__.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 """ A Pure Data to Python Utility
 """
+
+from .util.utils import *
+
 from .classes.classes import *
+from .classes.default import *
 from .classes.translator import *
 from .classes.pdpy import *
-from .util.utils import *
+
 from .parse.getters import *
 from .parse.json2pd import *
 from .parse.pd2json import *
