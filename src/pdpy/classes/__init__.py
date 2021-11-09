61e376d709e742028a2768906f64c696738f27c5
fdch <fch226@nyu.edu>
2021-11-09 15:13:28 -0300

    fix syntax and init file

--- src/pdpy/classes/__init__.py
+++ src/pdpy/classes/__init__.py
@@ -8,6 +8,9 @@ from .default import *
 from .classes import *
 from .comment import *
 from .connections import *
+from .pdobject import *
+from .pdarray import *
+from .gui import *
 from .data_structures import *
 from .canvas import *
 from .iemgui import *
--- src/pdpy/classes/classes.py
+++ src/pdpy/classes/classes.py
@@ -11,7 +11,7 @@ __all__ = [
   "Coords",
   "Graph",
   "Point",
-  "PdObj"
+  "PdObj",
   "Size"
 ]
 
