b1ec2da17604916526c2b4aa7f8d70334efc79d4
fdch <fch226@nyu.edu>
2021-11-10 22:03:31 -0300

    included missing classes
    PdPyEncoder: also refactor for json_dict input

--- src/pdpy/__init__.py
+++ src/pdpy/__init__.py
@@ -3,12 +3,33 @@
 """ A Pure Data to Python Utility
 """
 
+# The utilities
+
 from .util.utils import *
 
+# The classes
+
+from .classes.base import *
 from .classes.classes import *
+from .classes.canvas import *
 from .classes.default import *
-from .classes.translator import *
 from .classes.pdpy import *
+from .classes.comment import *
+from .classes.connections import *
+from .classes.data_structures import *
+from .classes.default import *
+from .classes.dependencies import *
+from .classes.gui import *
+from .classes.iemgui import *
+from .classes.message import *
+from .classes.pdarray import *
+from .classes.pdobject import *
+from .classes.translator import *
+
+# The parsers
 
 from .parse.json2pd import *
 from .parse.pd2json import *
+from .parse.json2xml import *
+from .parse.xml2json import *
+from .parse.parser import *
