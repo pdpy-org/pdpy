0595ab1b7b2c0148c416d4261507a3d3e07f80b6
fdch <fch226@nyu.edu>
2021-11-15 19:29:52 -0300

    PdObj resides in its own class now

--- src/pdpy/classes/classes.py
+++ src/pdpy/classes/classes.py
@@ -4,14 +4,13 @@
 """ Class Definitions """
 
 from .base import Base
-from .data_structures import PdType, PdData
+from .data_structures import PdType
 
 __all__ = [ 
   "Base",
   "Coords",
   "Graph",
   "Point",
-  "PdObj",
   "Size",
   "Bounds",
   "Area",
@@ -183,26 +182,4 @@ class Graph(Base):
       'size' : argv[1]
     }))
 
-class PdObj(PdData):
-  """ A PdObj base class 
-  
-  Description
-  -----------
-  A PdObj holds the id, and the x and y coordinates of the pd object,
-  as well as the arguments array.
 
-  Methods
-  -------
-  - `addargs(argv)`: Adds arguments to the pd object.
-  """
-  def __init__(self, id, x, y, **kwargs):
-    self.__pdpy__ = self.__class__.__name__
-    self.id = int(id)
-    self.position = Point(x=x, y=y)
-    super().__init__(**kwargs)
-  
-  def addargs(self, argv):
-    if not hasattr(self,'args') or self.args is None: 
-      self.args = []
-    for arg in argv:
-      self.args += [arg]
