68a1e8dd8df4e1125a689881b107882877285dd2
fdch <fch226@nyu.edu>
2021-11-17 18:47:19 -0300

    __pd__ method in PdData

--- src/pdpy/classes/pddata.py
+++ src/pdpy/classes/pddata.py
@@ -10,12 +10,25 @@ class PdData(Base):
   """ A PdData base class """
   def __init__(self, data=None, dtype=float, char=None, head=None):
     self.__pdpy__ = self.__class__.__name__
+    super().__init__(pdtype='A')
+
     self.dtype = dtype
     self.char = char
     if head is not None:
-      self.head = head
+      self.__cls__ = head
+    else:
+      self.__cls__ = ''
     if self.char is not None:
       self.data = splitByEscapedChar(data, char=self.char)
     else:
       self.data = [self.dtype(d) for d in data]
-  
+
+  def __pd__(self):
+    """ Parses the pd object into a string """
+    if hasattr(self, 'data'):
+      s = ''
+      for d in self.data:
+        s += f" {d}"
+      return super().__pd__(s)
+    else:
+      return ''
