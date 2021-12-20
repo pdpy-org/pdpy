f815914eedcec0cbb1b3f49a618996eb3dc75d17
fdch <fch226@nyu.edu>
2021-12-20 12:53:53 -0300

    fix tag to pd name routines

--- src/pdpy/classes/default.py
+++ src/pdpy/classes/default.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """ Pure Data Default Definitions """
+import re
 
 __all__ = [
   'Namespace',
@@ -83,28 +84,55 @@ class XmlTagConvert(object):
       '>>' : 'binop_ls',
       '<<' : 'binop_rs'
     }
+    self.tilde = "~"
+    self.__tilde__ = "_tilde"
+  
+  def find(self, element, string):
+    result = element in string
+    # print('find', element, string, result)
+    return result
 
   def to_tilde(self, tag):
     """ Returns the tag name replacing tilde char with _tilde """
-    if '~' in tag:
-      return tag.replace("~", "_tilde")
-    else:
+    
+    if not self.find(self.tilde, tag):
       return tag
+    s = str(tag).replace(self.tilde, self.__tilde__)
+    # print(f"to_tilde(): {tag} --> {s}")
+    return s
 
   def from_tilde(self, tag):
     """ Returns the tag name replacing _tilde with tilde char """
-    if '_tilde' in tag:
-      return tag.replace("_tilde", "~")
-    else:
+    if not self.find(self.__tilde__, tag):
       return tag
+    s = str(tag).replace(self.__tilde__, self.tilde)
+    # print(f"from_tilde(): {tag} --> {s}")
+    return s
 
-  def to_xml_tag(self, tag):
+  def to_xml_tag(self, key):
     """ Returns the tag name replacing special characters """
-    _tag = self.to_tilde(tag)
-    for key, value in self.table.items():
-      if key in _tag:
-        _tag = _tag.replace(key, value) 
-    return _tag
+    # print(f"to_xml_tag(): {key}")
+    tag = key
+    if self.find(self.tilde, key):
+      key_notilde = str(key).replace(self.tilde, '')
+      if key_notilde in self.table:
+        tag = self.table[key_notilde]
+      else:
+        # print(f'to_xml_tag(): tilde {key} not in table:', key)
+        pass
+      tag = self.to_tilde(tag)
+    else:
+      if key in self.table:
+        tag = self.table[key]
+      else:
+        for k, v in self.table.items():
+          if k in key:
+            tag = v
+            break
+        # print('to_xml_tag(): key not in table:', key)
+        tag = key
+    # print(f"to_xml_tag(): {key} --> {tag}")
+    return tag
 
 
   def to_pd_obj(self, pd_key):
@@ -236,6 +264,9 @@ class Default(object):
         'value':0,
         'steady':0
       }
+    }
+    self.xml = {
+      'data_as_text': False
     }
 
 
