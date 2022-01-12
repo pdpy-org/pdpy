d28cefdcb32c4b3d239d5a0444899fcb2f6a56a4
fdch <fch226@nyu.edu>
2022-01-12 18:00:14 -0300

    definitions in utilities

--- src/tests/utilities.sh
+++ src/tests/utilities.sh
@@ -6,15 +6,19 @@ CODEBASE=$(find $CODEPATH -name "*.py")
 function classes_and_methods {
   local file="$1"
   date > $file
-
+  # touch /tmp/temp
   for i in $CODEBASE
   do
     n=$(cat $i | wc -l | tr -d ' ')
     echo $i "<---------[ $n lines ]" >> $file
     grep -n "^class" $i >> $file
+    # grep "^class" $i | awk '{printf "%s pass\n",$0}' >> /tmp/temp
     grep -n "def " $i >> $file
     echo === >> $file
   done
+  # sort /tmp/temp > $file
+  # rm /tmp/temp
 }
+
 # get all the classes and method definitions into the given file
 classes_and_methods "definitions.txt"
