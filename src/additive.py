ed6f33e0b6de9a1cf1bb4e66114976c2104d7ad6
fdch <fch226@nyu.edu>
2022-08-02 22:09:48 -0300

    make a complex patch

--- src/additive.py
+++ src/additive.py
@@ -6,33 +6,77 @@
 # **************************************************************************** #
 from pdpy import PdPy, Obj
 from math import factorial, pow, exp, cos
+import random
 
 def poisson(k, l=6): 
   return ( pow(l, k) / factorial(k) ) * exp(-k)
 
 # initialize a patch (ie, a pdpy root patch)
-mypatch = PdPy(name="additive", root=True)
-# initialize an oscillator, a range and a dac with 4 channels
-partials = 10
+pd = PdPy(name="additive", root=True)
 
-def makephasors(i):
-  coef = poisson(i)/partials
-  objects = [
-    Obj('phasor~').addargs(coef * 10, cos(coef)),
-    Obj('*~').addargs(coef * 50),
-    Obj('+~').addargs(coef * 800),
-    Obj('osc~'),
-    Obj('*~').addargs(coef)
-  ]
-  mypatch.create(*objects)
-  mypatch.connect(*objects)
-  mypatch.connect(objects[-1], [dac, 0, 1])
+partials = 25
 
 dac = Obj('dac~')
-mypatch.create(dac)
+pd.create(dac)
 
-for i in range(partials):
-  makephasors(i)
+for i in range(1, partials):
+  p = poisson(i) / partials
+  rand = random.random()
+  objects = [
+    Obj('phasor~').addargs(p * 10, cos(p)),
+    Obj('lop~').addargs(100),
+    Obj('*~').addargs(p * 800),
+    Obj('+~').addargs(p * 800 + 500),
+    # phased out oscillators
+    Obj('phasor~'),
+    Obj('+~').addargs(cos(1-p)),
+    Obj('cos~'),
+    # add here some tiny waveshaping 
+    Obj('*~').addargs(2),
+    Obj('-~').addargs(1),
+    Obj('clip~').addargs(-0.3, 0.3),
+    Obj('*~').addargs(p),
+    Obj('*~'),
+    Obj('*~').addargs(1-rand),
+    Obj('*~').addargs(rand)
+  ]
+  pd.create(*objects)
+  pd.connect(*objects[:-2])
+  # add an envelope triggered by the 1st phasor (1-phasor)
+  oneminus = [
+    Obj('sig~').addargs(1),
+    Obj('-~')
+  ]
+  pd.create(*oneminus)
+  pd.connect(*oneminus)
+  # make it a quartic envelope
+  # using a convoluted programmatical way:
+  #
+  prev_m = Obj('*~')
+  pd.create(prev_m)
+  pd.connect(objects[1], [prev_m, 0, 1])
+  for x in range(4):
+    next_m = Obj('*~')
+    pd.create(next_m)
+    pd.connect(prev_m, [next_m, 0, 1])
+    prev_m = next_m
+  pd.connect(prev_m, [oneminus[-1], 1])
+  
+  # or this direct way:
+  # 
+  # mul = Obj('*~')
+  # mul2 = Obj('*~')
+  # pd.create(mul, mul2)
+  # pd.connect(objects[1], [mul, 0, 1])
+  # pd.connect(mul, [mul2, 0, 1])
+  # pd.connect(mul2, [oneminus[-1], 1])
+
+  pd.connect(oneminus[-1], [objects[-3], 1]) 
+  # plug it to the dac, in stereo
+  pd.connect(objects[-3], objects[-2])
+  pd.connect(objects[-3], objects[-1])
+  pd.connect(objects[-1], [dac, 0])
+  pd.connect(objects[-2], [dac, 1])
 
 # write out the patch
-mypatch.write()
+pd.write()
