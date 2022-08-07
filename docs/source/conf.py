1b4116683833ee4c312287b480f6c6b9fe639408
fdch <fch226@nyu.edu>
2022-08-07 20:36:28 -0300

    fix static paths, add image, and sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,5 +23,31 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
+
 html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+
+# If false, no module index is generated.
+html_domain_indices = True
+
+# If false, no index is generated.
+html_use_index = True
+
+html_use_modindex = True
+
+
+html_logo = 'img/pdpy-logo.png'
+
+html_theme_options = {
+    'logo_only': True,
+    'style_nav_header_background': 'white',
+    # 'analytics_id': '',
+}
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
